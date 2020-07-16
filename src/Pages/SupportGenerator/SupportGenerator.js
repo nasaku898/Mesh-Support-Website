fa76174552bdc2b8c2206ac9198300b529c8ad63
nasaku898 <simonlim898@hotmail.com>
2020-07-16 16:20:40 -0400

    #7 added canvas page

---
+++ src/Pages/SupportGenerator/SupportGenerator.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import STLCanvas from '../../Components/STLCanvas/STLCanvas'
+
+const SupportGenerator = () => {
+    return (
+        <>
+            <STLCanvas></STLCanvas>
+        </>
+    )
+}
+
+export default SupportGenerator
