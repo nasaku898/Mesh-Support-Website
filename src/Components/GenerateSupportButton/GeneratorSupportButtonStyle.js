bcb88b02aa3fc1d82b9ddd1dc22ab530bfecaf81
nasaku898 <simonlim898@hotmail.com>
2020-07-20 16:33:44 -0400

    #11 isolate generate support button

---
+++ src/Components/GenerateSupportButton/GeneratorSupportButtonStyle.js
@@ -0,0 +1,13 @@
+const { makeStyles } = require("@material-ui/core");
+
+const useStyles = makeStyles((theme) => ({
+    removeBtn: {
+        width: "100%"
+    },
+
+    progressBar: {
+        marginTop: "10px"
+    },
+}))
+
+export default useStyles
