6b5af8ab59f7e2989b8c7e13816d15c05a36b1cc
nasaku898 <simonlim898@hotmail.com>
2020-07-24 11:03:27 -0400

    #15 added translate, rotate and scale feature

---
+++ src/Components/TransformControls/TransformControls.js
@@ -0,0 +1,137 @@
+import React, { useState, useContext, useEffect } from 'react'
+import { Switch, IconButton, Typography, MenuItem, Menu, ButtonGroup, Button } from '@material-ui/core'
+import { CanvasContext } from '../../Utils/Context/CanvasContext'
+import { TransformControls as TransformManipulation } from 'three/examples/jsm/controls/TransformControls'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import AddIcon from '@material-ui/icons/Add'
+import RemoveIcon from '@material-ui/icons/Remove'
+
+const TransformControls = () => {
+    const [enableManipulation, setEnableManipulation] = useState(false)
+    const { scene, camera, renderer, sceneNames, orbitControls } = useContext(CanvasContext)
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
+    const toggleManipulation = () => {
+        if (!enableManipulation) {
+            const mesh = scene.current.getObjectByName(sceneNames.current.stlModel)
+            const transformControls = new TransformManipulation(camera.current, renderer.current.domElement)
+
+            transformControls.addEventListener('dragging-changed', (event) => { orbitControls.current.enabled = !event.value })
+            transformControls.attach(mesh)
+
+            sceneNames.current.transformControls = "transformControls"
+            transformControls.name = sceneNames.current.transformControls
+
+            scene.current.add(transformControls)
+            setEnableManipulation(true)
+        } else {
+            removeTransformControls()
+        }
+
+    }
+
+    useEffect(() => {
+        return () => {
+            removeTransformControls()
+        }
+        // eslint-disable-next-line
+    }, [])
+
+    const removeTransformControls = () => {
+        const transformControls = scene.current.getObjectByName(sceneNames.current.transformControls)
+        scene.current.remove(transformControls)
+        setEnableManipulation(false)
+    }
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleTranslation = () => {
+        const transformControls = scene.current.getObjectByName(sceneNames.current.transformControls)
+        transformControls.setMode("translate")
+    }
+
+    const handleRotation = () => {
+        const transformControls = scene.current.getObjectByName(sceneNames.current.transformControls)
+        transformControls.setMode("rotate")
+    }
+
+    const handleScale = () => {
+        const transformControls = scene.current.getObjectByName(sceneNames.current.transformControls)
+        transformControls.setMode("scale")
+    }
+
+    const handleIncrease = () => {
+        const transformControls = scene.current.getObjectByName(sceneNames.current.transformControls)
+        transformControls.setSize(transformControls.size + 0.1)
+    }
+
+    const handleDecrease = () => {
+        const transformControls = scene.current.getObjectByName(sceneNames.current.transformControls)
+        transformControls.setSize(Math.max(transformControls.size - 0.1, 0.1))
+    }
+    
+    return (
+        <>
+            <Typography> Manipulation</Typography>
+            <Switch
+                checked={enableManipulation}
+                onChange={toggleManipulation}
+                name="checkedA"
+                inputProps={{ 'aria-label': 'secondary checkbox' }}
+            />
+
+            {
+                (enableManipulation) &&
+                <IconButton
+                    aria-label="more"
+                    aria-controls="long-menu"
+                    aria-haspopup="true"
+                    onClick={handleClick}
+                >
+                    <MoreVertIcon />
+                </IconButton>
+            }
+
+            <Menu
+                id="long-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={open}
+                onClose={handleClose}
+                PaperProps={{
+                    style: {
+                        maxHeight: 48 * 4.5,
+                        width: '20ch',
+                    },
+                }}
+            >
+
+                <MenuItem onClick={handleTranslation}>
+                    Translate
+                </MenuItem>
+                <MenuItem onClick={handleRotation}>
+                    Rotation
+                </MenuItem>
+                <MenuItem onClick={handleScale}>
+                    Scale
+                </MenuItem>
+                <MenuItem>
+                    <ButtonGroup size="small" aria-label="small outlined button group">
+                        <Button onClick={handleIncrease}><AddIcon /></Button>
+                        <Button onClick={handleDecrease}><RemoveIcon /></Button>
+                    </ButtonGroup>
+                </MenuItem>
+            </Menu>
+        </>
+    )
+}
+
+export default TransformControls
